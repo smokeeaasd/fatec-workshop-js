4d6a05b45e1b878eed085ef7066dcd966f385943
Lucas <83725986+smokeeaasd@users.noreply.github.com>
2024-09-10 15:12:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ x += 10 // corrigindo o operador de atribuição de adição para aumentar o val
 
 // função lambda pra calcular a soma
 // dessa maneira, é possível criar funções simples usando apenas uma linha.
-const somar = (a, b) => a + b
+// adicionando parâmetros na função, para reutilizá-la com vários números
+const somar = (n1, n2) => n1 + n2
 
 console.log(somar(x, y))
